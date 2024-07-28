4a9c691b9196269d1af33304097249ebc60c87e9
Roeliefantje <roel.de.jeu@gmail.com>
2024-07-28 14:05:19 +0200

    feat: Allow for shared shader pipelines in object groups which can be used by induvidual objects with their own vertices

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,136 @@
-use wgpu::{core::{command::ClearError, instance}, hal::gles::Adapter, rwh::AppKitDisplayHandle};
+use wgpu::{core::{command::ClearError, instance}, hal::gles::Adapter, rwh::AppKitDisplayHandle, util::DeviceExt};
 use winit::{
     window::Window,
     event::*,
 };
 
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3]
+}
+
+impl Vertex {
+
+    const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0=> Float32x3, 1 => Float32x3];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.2, 0.0, 0.2] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.4, 0.0, 0.4] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.6, 0.0, 0.6] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.8, 0.0, 0.8] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [1.0, 0.0, 1.0] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
+
+struct Object {
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_vertices: u32,
+}
+
+impl Object {
+    pub fn new(device: &wgpu::Device) -> Object {
+        Self {
+            vertex_buffer: device.create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer"),
+                    contents: bytemuck::cast_slice(VERTICES),
+                    usage: wgpu::BufferUsages::VERTEX,
+                }
+            ),
+            index_buffer: device.create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Index Buffer"),
+                    contents: bytemuck::cast_slice(INDICES),
+                    usage: wgpu::BufferUsages::INDEX,
+                }
+            ),
+            num_vertices: INDICES.len() as u32
+        }
+    }
+}
+
 //An Object Group shares its shaders with other objects.
 //These objects can have their own meshes and will have seperated draw calls.
 struct ObjectGroup {
     render_pipeline: wgpu::RenderPipeline,
+    objects: Vec<Object>
+}
+
+impl ObjectGroup {
+    pub fn new(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration) -> ObjectGroup {
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Object Group Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                buffers: &[
+                    Vertex::desc(),
+                ]
+            },
+            fragment: Some(wgpu::FragmentState { module: &shader, entry_point: "fs_main", 
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })]
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState { count: 1, mask: !0, alpha_to_coverage_enabled: false },
+            multiview: None,
+        });
+
+        let mut objects: Vec<Object> = Vec::new();
+
+        objects.push(Object::new(device));
+        objects.push(Object::new(device));
+        objects.push(Object::new(device));
+        objects.push(Object::new(device));
+        
+
+        Self {
+            render_pipeline,
+            objects
+        }
+    }
 }
 
 //An InstanceGroup shares its shaders and mesh with multiple objects.
@@ -82,8 +205,10 @@ impl <'a> State<'a > {
             a: 1.0,
         };
 
-        let object_groups: Vec<ObjectGroup> = Vec::new();
+        let mut object_groups: Vec<ObjectGroup> = Vec::new();
         
+        object_groups.push(ObjectGroup::new(&device, &config));
+
         Self {
             surface,
             device,
@@ -130,7 +255,7 @@ impl <'a> State<'a > {
 
         //Clear the screen
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -144,9 +269,20 @@ impl <'a> State<'a > {
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
+
+            //TODO: Render all the objects here...
+            for object_group in &self.object_groups {
+                render_pass.set_pipeline(&object_group.render_pipeline);
+                
+                for object in &object_group.objects {
+                    render_pass.set_vertex_buffer(0, object.vertex_buffer.slice(..));
+                    render_pass.set_index_buffer(object.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                    render_pass.draw_indexed(0..object.num_vertices, 0, 0..1);
+                }
+            }
         }
 
-        //TODO: Render all the objects here...
+        
         
 
         self.queue.submit(std::iter::once(encoder.finish()));
